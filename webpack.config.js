f8e4679f432bf4bd4187e19f85ad4fcd1b5ce5a1
Hob Adams <hello@hobadams.com>
2019-11-28 11:57:33 +0000

    add fixes to auto loading

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   entry: "./src/js/index.js",
   output: {
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js"
     },
   
   watch: true,
